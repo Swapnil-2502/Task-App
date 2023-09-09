c319f766c4732911f25da9c09f5e3602ecd527e6
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2023-09-09 20:46:04 +0530

    modified few files and added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // template: "<h1>This is my First Angular App Hello</h1>"
 })
 export class AppComponent {
-  title = 'Swapnil-app';
 }
